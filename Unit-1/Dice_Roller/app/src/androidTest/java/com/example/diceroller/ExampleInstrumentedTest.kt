41da92f1c9937b10a510ae86a1b343ec37865a98
BHARATH VISHNU C J <98733219+cjbharath@users.noreply.github.com>
2022-09-25 16:22:45 +0530

    Add files via upload

---
+++ Unit-1/Dice_Roller/app/src/androidTest/java/com/example/diceroller/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package com.example.diceroller
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.diceroller", appContext.packageName)
+    }
+
+}
